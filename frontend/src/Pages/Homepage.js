7e993f658c06e4cc42e8e9a241d6811db7063772
RidhikaJoshi <ridhikasmrh@gmail.com>
2023-08-21 14:29:30 +0530

    Created Login and Sign Up Tabs for the Homepage

--- frontend/src/Pages/Homepage.js
+++ frontend/src/Pages/Homepage.js
@@ -1,9 +1,52 @@
-import React from 'react'
+import React from "react";
+import {
+	Container,
+	Box,
+	Text,
+	Tabs,
+	TabList,
+	TabPanels,
+	Tab,
+	TabPanel,
+} from "@chakra-ui/react";
+
+import Login from "../components/Authentication/Login";
+import Signup from "../components/Authentication/Signup";
 
 const Homepage = () => {
-  return (
-    <div>Home</div>
-  );
-}
+	return (
+		<Container maxW="xl" centerContent>
+			<Box
+				d="flex"
+				justify-content="center"
+				p={3}
+				bg={"white"}
+				w="100%"
+				margin="40px 0 15px 0"
+				borderRadius="lg"
+				borderWidth="1px"
+				textAlign={"center"}
+			>
+				<Text fontSize="3xl" fontFamily="Satisfy">
+					ChatFusion
+				</Text>
+			</Box>
+			<Box bg="white" w="100%" p={4} borderRadius="lg" borderWidth="1px">
+				<Tabs variant="soft-rounded">
+					<TabList mb="1em">
+						<Tab width="50%">Login</Tab>
+						<Tab width="50%">Sign Up</Tab>
+					</TabList>
+					<TabPanels>
+						<TabPanel>{<Login />}</TabPanel>
+						<TabPanel>
+							<Signup />
+						</TabPanel>
+					</TabPanels>
+				</Tabs>
+			</Box>
+		</Container>
+	);
+};
 
-export default Homepage
+export default Homepage;
