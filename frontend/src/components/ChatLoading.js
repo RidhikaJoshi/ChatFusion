4895d91566350173a103a98aae06aa21775f8e32
RidhikaJoshi <ridhikasmrh@gmail.com>
2023-08-26 22:23:32 +0530

    Added ChatLoading Page

---
+++ frontend/src/components/ChatLoading.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Skeleton, Stack } from "@chakra-ui/react";
+
+const ChatLoading = () => {
+	return (
+		<Stack>
+			<Skeleton height="40px" />
+			<Skeleton height="40px" />
+			<Skeleton height="40px" />
+			<Skeleton height="40px" />
+			<Skeleton height="40px" />
+			<Skeleton height="40px" />
+			<Skeleton height="40px" />
+			<Skeleton height="40px" />
+			<Skeleton height="40px" />
+			<Skeleton height="40px" />
+			<Skeleton height="40px" />
+			<Skeleton height="40px" />
+		</Stack>
+	);
+};
+
+export default ChatLoading;
